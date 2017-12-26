ed8edcb44d332681985246d5e64ec7fd4e772ae1
Bartosz Ufnal <ufnalbartosz@gmail.com>
2017-12-26 01:08:04 +0100

    Udated tensorflow with GPU support (cuda8.0&cudnn6.0)

--- setup_env.sh
+++ setup_env.sh
@@ -3,38 +3,41 @@ export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
 sudo dpkg-reconfigure locales
 
-# install pip and emacs
-sudo apt-get install python-pip
-sudo apt-get install emacs
-pip install --user --upgrade pip
+# download and install conda
+curl -O https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
+bash Anaconda3-4.2.0-Linux-x86_64.sh
 
-# download cudnn-8.0-linux-x64-v5.1.tgz
-tar xvcf cudnn*
-sudo cp cuda/include/cudnn.h /usr/local/cuda/include
-sudo cp cuda/lib64/libcudnn* /usr/local/cuda/include
-sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
+# prepare conda env
+conda create -n tensorflow python=3.6
 
-# edit .profile
-export CUDA_HOME=/usr/local/cuda
-export LD_LIBRATY_PATH=/usr/local/cuda/lib64
+# prepare tensorflow with GPU support
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt-get update
+sudo apt-get install build-essential
 
-pip install --user tensorflow_gpu
-pip install --user tflearn
-pip install --user scipy
+sudo apt-get install python-dev python3-dev python-virtualenv
 
+# install nvidia driver
+sudo apt-get install nvidia-387
+sudo apt-get install mesa-common-dev
+sudo apt-get install freeglut3-dev
 
-# clone repo
-git clone https://github.com/ufnalbartosz/project_cnn
+# install cuda 8.0
+wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run
+sudo bash cuda_8.0.61_375.26_linux-run
 
+# export cuda paths
+export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
-# setup script
-#!/bin/bash
-echo "Checking for CUDA and installing."
-# Check for CUDA and try to install.
-if ! dpkg-query -W cuda; then
-    # The 16.04 installer works with 16.10.
-    curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-    dpkg -i ./cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-    apt-get update
-    apt-get install cuda -y
-fi
+# install cudnn
+wget https://developer.nvidia.com/compute/machine-learning/cudnn/secure/v6/prod/8.0_20170307/cudnn-8.0-linux-x64-v6.0-tgz
+tar -zxvf cudnn-8.0-linux-x64-v6.0-ga.tgz
+sudo cp cuda/include/cudnn.h /usr/local/cuda/include/
+sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64/
+sudo chmod a+r /usr/local/cuda/include/cudnn.h
+sudo chmod a+r /usr/local/cuda/lib64/libcudnn*
+
+# install tf-gpu
+source activate tensorflow
+pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.4.0-cp36-cp36m-linux_x86_64.whl
