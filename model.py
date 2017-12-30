3bb642bc660f49ead96ff9c322490dd1a70d0076
Bartosz Ufnal <ufnalbartosz@gmail.com>
2017-12-30 23:20:00 +0100

    3_3 convolution without local response normaliztion

--- model.py
+++ model.py
@@ -13,23 +13,24 @@ data = DataSet().maybe_download_and_extract()
 
 # Building 'AlexNet'
 network = input_data(shape=[None, 227, 227, 3], name='input')
-network = conv_2d(network, 96, 11, strides=4,
-                  activation='relu', name='conv1_11_11')
+network = conv_2d(network, 96, 7, strides=1,
+                  activation='relu', name='conv1_7_7')
 network = max_pool_2d(network, 3, strides=2, name='max_pool1_3_3_2')
-network = local_response_normalization(network)
-network = conv_2d(network, 256, 5, activation='relu', name='conv2_5_5')
+# network = local_response_normalization(network)
+network = conv_2d(network, 256, 3, activation='relu', name='conv2_3_3')
+network = conv_2d(network, 256, 3, activation='relu', name='conv3_3_3')
 network = max_pool_2d(network, 3, strides=2, name='max_pool2_3_3_2')
-network = local_response_normalization(network)
+# network = local_response_normalization(network)
 network = conv_2d(network, 384, 3, activation='relu', name='conv3_3_3')
 network = conv_2d(network, 384, 3, activation='relu', name='conv4_3_3')
-network = conv_2d(network, 256, 3, activation='relu', name='conv5_3_3')
+# network = conv_2d(network, 256, 3, activation='relu', name='conv5_3_3')
 network = max_pool_2d(network, 3, strides=2, name='max_pool3_3_3_2')
-network = local_response_normalization(network)
+# network = local_response_normalization(network)
 network = fully_connected(
-    network, 4096, activation='tanh', name='fully_connected_1_tanh')
+    network, 4096, activation='relu', name='fully_connected_1_relu')
 network = dropout(network, 0.5, name='dropout_1_05')
 network = fully_connected(
-    network, 4096, activation='tanh', name='fully_connected_2_tanh')
+    network, 4096, activation='relu', name='fully_connected_2_relu')
 network = dropout(network, 0.5, name='dropout_2_05')
 network = fully_connected(
     network, 17, activation='softmax', name='fully_connected_3_softmax')
@@ -37,27 +38,26 @@ network = regression(network, optimizer='momentum',
                      loss='categorical_crossentropy',
                      learning_rate=0.001,
                      name='target')
-
+model_name = '3_3'
 # Training
 model = tflearn.DNN(network,
-                    checkpoint_path='checkpoints/model',
+                    checkpoint_path='checkpoints/{}'.format(model_name),
                     tensorboard_verbose=3,
                     tensorboard_dir='logs')
 
-model.load('saves/model')
-# model.load('checkpoints/model-2000')
+# model.load('saves/{}'.format(model_name))
 
-# model.fit({'input': data['train_images']},
-#           {'target': data['train_labels']},
-#           n_epoch=150,
-#           validation_set=({'input': data['valid_images']}, {
-#                           'target': data['valid_labels']}),
-#           shuffle=True,
-#           show_metric=True,
-#           batch_size=64,
-#           snapshot_step=500,
-#           snapshot_epoch=False,
-#           run_id='model')
+model.fit({'input': data['train_images']},
+          {'target': data['train_labels']},
+          n_epoch=150,
+          validation_set=({'input': data['valid_images']}, {
+                          'target': data['valid_labels']}),
+          shuffle=True,
+          show_metric=True,
+          batch_size=64,
+          snapshot_step=500,
+          snapshot_epoch=False,
+          run_id='model')
 
 predicted_labels = model.predict(data['test_images'])
 
@@ -71,5 +71,5 @@ accuracy = np.sum(np.equal(pred_labels, true_labels))
 
 num_test_images = data['test_images'].shape[0]
 print(accuracy / len(true_labels))
-# print('Saving model...')
-# model.save('saves/model')
+print('Saving model...')
+model.save('saves/{}'.format(model_name))
